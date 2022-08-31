ff7787970e2f0b1f9a5c08c87181f12aa309b0bb
Eddie Tang <eddietang2314@gmail.com>
2022-08-31 11:53:39 -0400

    more stuff

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -4,24 +4,42 @@ import 'package:zenesus/utils/cookies.dart';
 import 'package:zenesus/screens/faq.dart';
 import 'package:zenesus/serializers/students_name_and_id.dart';
 import 'package:vibration/vibration.dart';
+import 'package:zenesus/screens/studentpage.dart';
 
 class StudentAppBar extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => StudentAppBarState();
 }
 
+bool _ispressed = false;
+
 class StudentAppBarState extends State<StudentAppBar> {
+  Future<List<String>> onPressedDoStuff(int i) async {
+    await writeUserNumintoCookies(i);
+    if (await Vibration.hasVibrator() ?? false) {
+      Vibration.vibrate();
+    }
+    List<String> things = await readEmailPassSchoolintoCookies();
+    String email = things[0];
+    String password = things[1];
+    String school = things[2];
+    return [email, password, school];
+  }
+
   List<Widget> createChildren(BuildContext context, Student_Name_and_ID data) {
     List<Widget> myList = [];
     for (int i = 0; i < data.ids.length; i++) {
       myList.add(
         SimpleDialogOption(
-          onPressed: () async {
-            await writeUserNumintoCookies(i);
-            if (await Vibration.hasVibrator() ?? false) {
-              Vibration.vibrate();
-            }
-            return;
+          onPressed: () {
+            onPressedDoStuff(i).then((value) {
+              Navigator.of(context).pop();
+              const snackBar = SnackBar(
+                content: Text(
+                    "Success!😁 - go to grades and you will now see the other user's grades!"),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            });
           },
           child: Text('${data.names[i]} - ${data.ids[i]}'),
         ),
@@ -45,25 +63,39 @@ class StudentAppBarState extends State<StudentAppBar> {
       actions: <Widget>[
         Tooltip(
           message: "Change User",
-          child: IconButton(
-            icon: const Icon(Icons.person, color: Colors.white),
-            onPressed: () async {
-              List<String> things = await readEmailPassSchoolintoCookies();
-              String email = things[0];
-              String password = things[1];
-              String school = things[2];
-              Student_Name_and_ID futureNameandID =
-                  await createStudent_name_and_ID(email, password, school);
+          child: _ispressed
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [Text("Loading...")])
+              : IconButton(
+                  icon: const Icon(Icons.person, color: Colors.white),
+                  onPressed: () async {
+                    setState(() {
+                      _ispressed = true;
+                    });
 
-              await showDialog<Student_Name_and_ID>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return SimpleDialog(
-                        title: const Text('Select Student'),
-                        children: createChildren(context, futureNameandID));
-                  });
-            },
-          ),
+                    List<String> things =
+                        await readEmailPassSchoolintoCookies();
+                    String email = things[0];
+                    String password = things[1];
+                    String school = things[2];
+                    Student_Name_and_ID futureNameandID =
+                        await createStudent_name_and_ID(
+                            email, password, school);
+
+                    showDialog<Student_Name_and_ID>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return SimpleDialog(
+                              title: const Text('Select Student'),
+                              children:
+                                  createChildren(context, futureNameandID));
+                        });
+                    setState(() {
+                      _ispressed = false;
+                    });
+                  },
+                ),
         ),
         Tooltip(
           message: "FAQ page",
